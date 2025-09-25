6fef30f3c3a8691faa14257d3a2a5250f70f8261
MaiStar28 <hoangsaomai04@gmail.com>
2025-09-25 17:24:24 +0700

    update Part3

--- src/homework/Topic_05_TCs_TextboxTextArea_Part3.java
+++ src/homework/Topic_05_TCs_TextboxTextArea_Part3.java
@@ -2,12 +2,15 @@ package homework;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.time.Instant;
 import java.util.Random;
 
 public class Topic_05_TCs_TextboxTextArea_Part3 {
@@ -15,7 +18,7 @@ public class Topic_05_TCs_TextboxTextArea_Part3 {
     WebDriver driver;
     By myAcc = By.xpath("//div[@class='footer']//a[@title='My Account']");
     By createAcc = By.xpath("//a[@class='button']");
-    String firstName, lastName, fullName, emailAdd, pass;
+    String firstName, lastName, fullName, emailAdd, pass,userName,phoneNumber, comment;
     Random rand;
 
     @BeforeClass
@@ -27,8 +30,10 @@ public class Topic_05_TCs_TextboxTextArea_Part3 {
         lastName = "mai";
         fullName = firstName + " " + lastName;
         emailAdd = "hoang" + rand.nextInt(999) + "@gmail.com";
-        pass = "1234567";
-
+        pass = "1234567m";
+        userName = "maiimaii1234";
+        phoneNumber = "0362646254";
+        comment = "Hoàng Mai share cmt\nVery good\nYes";
     }
     @Test
     public void TC_01_TechPanda() throws InterruptedException {
@@ -56,16 +61,82 @@ public class Topic_05_TCs_TextboxTextArea_Part3 {
         driver.findElement(By.xpath("//h2/a[@title='Samsung Galaxy']")).click();
         driver.findElement(By.xpath("//a[text()='Add Your Review']")).click();
         driver.findElement(By.xpath("//input[@id='Quality 1_5']")).click();
-        driver.findElement(By.xpath("//textare[@id='review_field']")).sendKeys("Very good\n from Mai");
+        driver.findElement(By.xpath("//textarea[@id='review_field']")).sendKeys("Very good\n from Mai");
         driver.findElement(By.xpath("//input[@id='summary_field']")).sendKeys("Good Phone");
         driver.findElement(By.xpath("//button[@title='Submit Review']")).click();
-        Assert.assertEquals(driver.findElement(By.cssSelector("li.success-msg span")).getText(),"Your review has been accept for moderation.");
+        Assert.assertEquals(driver.findElement(By.cssSelector("li.success-msg span")).getText(),"Your review has been accepted for moderation.");
 
         //logout
         driver.findElement(By.cssSelector("div.account-cart-wrapper a")).click();
         driver.findElement(By.xpath("//li[@class=' last']")).click();
         Thread.sleep(6000);
         Assert.assertEquals(driver.getCurrentUrl(), "http://live.techpanda.org/index.php/");
+
+    }
+    @Test
+    public void TC_02_OrangeHRM() throws InterruptedException {
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//input[@name='username']")).sendKeys("Admin");
+        driver.findElement(By.xpath("//input[@name='password']")).sendKeys("admin123");
+        driver.findElement(By.xpath("//button[@class='oxd-button oxd-button--medium oxd-button--main orangehrm-login-button']")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//span[text()='PIM']")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//a[text()='Add Employee']")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//input[@name='firstName']")).sendKeys(firstName);
+        driver.findElement(By.xpath("//input[@name='lastName']")).sendKeys(lastName);
+        String employeeID = driver.findElement(By.xpath("//label[text()='Employee Id']/parent::div/following-sibling::div/input")).getAttribute("value");
+        driver.findElement(By.xpath("//div[@class='oxd-switch-wrapper']")).click();
+        driver.findElement(By.xpath("//label[text()='Username']/parent::div/following-sibling::div/input")).sendKeys(userName);
+        driver.findElement(By.xpath("//label[text()='Password']/parent::div/following-sibling::div/input")).sendKeys(pass);
+        driver.findElement(By.xpath("//label[text()='Confirm Password']/parent::div/following-sibling::div/input")).sendKeys(pass);
+        driver.findElement(By.xpath("//button[@class='oxd-button oxd-button--medium oxd-button--secondary orangehrm-left-space']")).click();
+        Thread.sleep(9000);
+        Assert.assertEquals(driver.findElement(By.xpath("//input[@name='firstName']")).getAttribute("value"), firstName);
+        Assert.assertEquals(driver.findElement(By.xpath("//input[@name='lastName']")).getAttribute("value"), lastName);
+        Assert.assertEquals(driver.findElement(By.xpath("//label[text()='Employee Id']/parent::div/following-sibling::div/input")).getAttribute("value"), employeeID);
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//a[text()='Immigration']")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//h6[text()='Assigned Immigration Records']/following-sibling::button")).click();
+        driver.findElement(By.xpath("//label[text()='Number']/parent::div/following-sibling::div/input")).sendKeys(phoneNumber);
+        driver.findElement(By.xpath("//textarea[@class='oxd-textarea oxd-textarea--active oxd-textarea--resize-vertical']")).sendKeys(comment);
+        driver.findElement(By.xpath("//button[@class='oxd-button oxd-button--medium oxd-button--secondary orangehrm-left-space']")).click();
+        Thread.sleep(7000);
+        driver.findElement(By.xpath("//i[@class='oxd-icon bi-pencil-fill']")).click();
+        Thread.sleep(3000);
+        Assert.assertEquals(driver.findElement(By.xpath("//label[text()='Number']/parent::div/following-sibling::div/input")).getAttribute("value"),phoneNumber);
+        Assert.assertEquals( driver.findElement(By.xpath("//textarea[@class='oxd-textarea oxd-textarea--active oxd-textarea--resize-vertical']")).getAttribute("value"),comment);
+
+        driver.findElement(By.xpath("//div[@class='oxd-topbar-header-userarea']")).click();
+        driver.findElement(By.xpath("//a[text()='Logout']")).click();
+
+        Thread.sleep(5000);
+        driver.findElement(By.xpath("//input[@name='username']")).sendKeys(userName);
+        driver.findElement(By.xpath("//input[@name='password']")).sendKeys(pass);
+        driver.findElement(By.xpath("//button[@class='oxd-button oxd-button--medium oxd-button--main orangehrm-login-button']")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//span[text()='My Info']")).click();
+        Thread.sleep(3000);
+        Assert.assertEquals(driver.findElement(By.xpath("//input[@name='firstName']")).getAttribute("value"), firstName);
+        Assert.assertEquals(driver.findElement(By.xpath("//input[@name='lastName']")).getAttribute("value"), lastName);
+        Assert.assertEquals(driver.findElement(By.xpath("//label[text()='Employee Id']/parent::div/following-sibling::div/input")).getAttribute("value"), employeeID);
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//a[text()='Immigration']")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//i[@class='oxd-icon bi-pencil-fill']")).click();
+        Thread.sleep(3000);
+        Assert.assertEquals(driver.findElement(By.xpath("//label[text()='Number']/parent::div/following-sibling::div/input")).getAttribute("value"),phoneNumber);
+        Assert.assertEquals( driver.findElement(By.xpath("//textarea[@class='oxd-textarea oxd-textarea--active oxd-textarea--resize-vertical']")).getAttribute("value"),comment);
+
+    }
+
+    @Test
+    public void TC_03_NopCommerce() throws InterruptedException {
+        driver.get("https://demo.nopcommerce.com/register");
+        driver.findElement(By.cssSelector("a.ico-register")).click();
 
     }
     @AfterClass
